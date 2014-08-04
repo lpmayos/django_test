c533a2ddcf80ca34c18ce380f0c338c743976bd9
lpmayos <lpmayos@gmail.com>
2014-08-04 13:49:07 +0200

    django tutorial part 2: https://docs.djangoproject.com/en/1.6/intro/tutorial02/

--- src/adventure_time/admin.py
+++ src/adventure_time/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
-from adventure_time.models import World
+from adventure_time.models import World, Location
 
 
-admin.site.register(World)
+class LocationInline(admin.TabularInline):
+    model = Location
+    extra = 3
+
+
+class WorldAdmin(admin.ModelAdmin):
+    # fields = ['name', 'surface', 'world_type']
+    fieldsets = [
+        (None, {'fields': ['name', 'world_type']}),
+        ('Other information', {'fields': ['surface'], 'classes': ['collapse']}),
+    ]
+    inlines = [LocationInline]
+    list_display = ('name', 'world_type', 'surface')
+    list_filter = ['world_type']
+    search_fields = ['name']
+
+admin.site.register(World, WorldAdmin)
